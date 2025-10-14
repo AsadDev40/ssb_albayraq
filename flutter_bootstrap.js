51503414fac2071e8a83823ef082ab2bf7689f00
AsadDev40 <ashuasadmalikmalik@gmail.com>
2025-10-13 23:55:31 -0700

    fix qr scanner issue

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "760865825"
+    serviceWorkerVersion: "3174392274"
   }
 });
