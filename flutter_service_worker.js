dac6021951a7c30be0778fe86fb5e50551b2df98
AsadDev40 <ashuasadmalikmalik@gmail.com>
2025-10-14 04:42:09 -0700

    fix scanning bugs

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9298e4c6e26bd94570be4d2489a398ca",
+const RESOURCES = {"flutter_bootstrap.js": "e3c5a91d3f71e0a5857292dff71e0b8b",
 "version.json": "f54e81b9cab9c2e7f53f815c85f1b381",
 "index.html": "5bd1e0160d024ce669bde02ebc3668d7",
 "/": "5bd1e0160d024ce669bde02ebc3668d7",
-"main.dart.js": "24fa564776053187e35d592b6914a34c",
+"main.dart.js": "1e4a4f8926818b83ff9dac1c8fae21a5",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "dd8233ab1e7e752b4f9e45a226b2a791",
 "icons/Icon-192.png": "197d86786ef90171f84ca2f7804ebbde",
