1e91ea267ac3e5229629e724494a9783383f92dd
AsadDev40 <ashuasadmalikmalik@gmail.com>
2025-12-15 19:23:25 +0300

    fix qr scanner screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c5bea65638088aed55584593332e2082",
+const RESOURCES = {"flutter_bootstrap.js": "c7ff44d8c096c55fba7661032dd4cf88",
 "version.json": "f54e81b9cab9c2e7f53f815c85f1b381",
 "index.html": "5bd1e0160d024ce669bde02ebc3668d7",
 "/": "5bd1e0160d024ce669bde02ebc3668d7",
-"main.dart.js": "3b39520d626c8888493a9b8fb6059889",
+"main.dart.js": "933d56ad47c9bec92b5d2abb8329367e",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "dd8233ab1e7e752b4f9e45a226b2a791",
 "icons/Icon-192.png": "197d86786ef90171f84ca2f7804ebbde",
